03d8ca79e6adeb76192c012536ba17cc3603e718
赤坂勝哉 <akasakatora1208@gmail.com>
2017-12-07 19:44:32 +0900

    first commit could drop T Block per second

---
+++ Assets/Resources/Scripts/GameController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	public static bool isGameStarted = false;	//if game play started, true
+	public static bool readyToStart = true;	//if stage created, true
+	//public GameObject text;
+	public static GameObject nowBlock;	//now dropping block
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnClick()
+	{
+		if (readyToStart) {
+			isGameStarted = true;
+			//text.SetActive (true);
+		}
+	}
+}
